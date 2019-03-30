a788b98e22887a0e35de14871d16232956ac0ef6
Richard Jarram <rjarram@me.com>
2019-03-29 22:11:02 -0400

    lol

--- battle.rb
+++ battle.rb
@@ -1,8 +1,4 @@
 def battle
-  kanga = (1..100).to_a.sample(1)
-  yellow_duck = (1..100).to_a.sample(1)
-  return "It's A Tie!" if kanga[0] == yellow_duck[0]
-  kanga[0] > yellow_duck[0] ? "The Zilla Wins!" : "The Duck Prevails!"
 end
 
 p battle
